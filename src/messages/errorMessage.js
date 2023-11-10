f352b623ac5422ced4146331954e5d4d58dd0894
hongjimin00 <chunsim015@gmail.com>
2023-11-10 17:14:41 +0900

    feat: 메뉴 입력 관련 에러 메세지 작성

--- src/messages/errorMessage.js
+++ src/messages/errorMessage.js
@@ -1,6 +1,13 @@
 const DATE_ERROR = Object.freeze({
   valid: "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.",
-  type: "[ERROR] 숫자만 입력 가능합니다.",
+  type: "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.",
 });
 
-module.exports = { DATE_ERROR };
+const MENU_ERROR = Object.freeze({
+  valid: "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.",
+  type: "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.",
+  form: "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.",
+  duplication: "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.",
+});
+
+module.exports = { DATE_ERROR, MENU_ERROR };
