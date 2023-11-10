478bf24553fa843fd55db5400dbace5f50c57983
hongjimin00 <chunsim015@gmail.com>
2023-11-10 15:16:34 +0900

    feat: 메뉴 입력받기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Console } from "@woowacourse/mission-utils";
+import InputView from "./InputView.js";
 
 class App {
   async run() {
     Console.print("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
-    const visitDate = await readDate();
+    const visitDate = await InputView.readDate();
+    const orderMenu = await InputView.readMenu();
   }
 }
 
