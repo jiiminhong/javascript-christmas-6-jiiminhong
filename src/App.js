a9901402032b4f0d8b179b041439a1da9e217560
hongjimin00 <chunsim015@gmail.com>
2023-11-10 15:06:49 +0900

    feat: 방문 날짜 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    Console.print("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+    const visitDate = await readDate();
+  }
 }
 
 export default App;
