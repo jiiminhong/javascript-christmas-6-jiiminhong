263c6fdf8e9bac0940e0cd4ac9bf025f1d7972ea
hongjimin00 <chunsim015@gmail.com>
2023-11-11 14:31:06 +0900

    refactor: constants 파일 수정

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,16 +1,16 @@
 const MENU_CONSTANTS = Object.freeze({
-  soup: { price: 6000, type: appetizer },
-  tapas: { price: 5500, type: appetizer },
-  salad: { price: 8000, type: appetizer },
-  steak: { price: 55000, type: main },
-  barbeque: { price: 54000, type: main },
-  seafoodPasta: { price: 35000, type: main },
-  christmasPasta: { price: 25000, type: main },
-  chocoCake: { price: 15000, type: dessert },
-  icecream: { price: 5000, type: dessert },
-  zeroCoke: { price: 3000, type: drink },
-  redWine: { price: 60000, type: drink },
-  champagne: { price: 25000, type: drink },
+  양송이수프: { price: 6000, type: appetizer },
+  타파스: { price: 5500, type: appetizer },
+  시저샐러드: { price: 8000, type: appetizer },
+  티본스테이크: { price: 55000, type: main },
+  바비큐립: { price: 54000, type: main },
+  해산물파스타: { price: 35000, type: main },
+  크리스마스파스타: { price: 25000, type: main },
+  초코케이크: { price: 15000, type: dessert },
+  아이스크림: { price: 5000, type: dessert },
+  제로콜라: { price: 3000, type: drink },
+  레드와인: { price: 60000, type: drink },
+  샴페인: { price: 25000, type: drink },
 });
 
 module.exports = { MENU_CONSTANTS };
