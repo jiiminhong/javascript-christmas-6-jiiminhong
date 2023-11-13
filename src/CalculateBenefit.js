61666fb412dc0dfe7941ac4f5f315e304a3e354b
hongjimin00 <chunsim015@gmail.com>
2023-11-14 00:23:18 +0900

    feat: 총금액 10000원 이상일 때 이벤트 적용되도록 추가

--- src/CalculateBenefit.js
+++ src/CalculateBenefit.js
@@ -1,7 +1,7 @@
 import { MENU_CONSTANTS } from "./constants/constants";
 
 const CalculateBenefit = {
-  calculateBenefit(visitDate, orderLis, totalBefore) {
+  calculateBenefit(visitDate, orderList, totalBefore) {
     const date = new Date(2023, 11, 1);
     let benefit = {
       christmasDiscount: { name: "크리스마스 디데이 할인", discount: 0 },
@@ -11,19 +11,21 @@ const CalculateBenefit = {
       giftEvent: { name: "증정 이벤트", discount: 0 },
     };
 
-    if (visitDate <= 25)
-      benefit.christmasDiscount.discount = 1000 + 100(visitDate - 1);
+    if (totalBefore >= 10000) {
+      if (visitDate <= 25)
+        benefit.christmasDiscount.discount = 1000 + 100(visitDate - 1);
 
-    if (date.getDay() >= 0 && date.getDay() <= 4)
-      calculateDessertDiscount(orderList);
+      if (date.getDay() >= 0 && date.getDay() <= 4)
+        calculateDessertDiscount(orderList);
 
-    if (date.getDay() === 5 || date.getDay() === 6)
-      calculateWeekendDiscount(orderList);
+      if (date.getDay() === 5 || date.getDay() === 6)
+        calculateWeekendDiscount(orderList);
 
-    if ([3, 10, 17, 24, 25, 31].includes(visitDate))
-      benefit.specialDiscount.discount = 1000;
+      if ([3, 10, 17, 24, 25, 31].includes(visitDate))
+        benefit.specialDiscount.discount = 1000;
 
-    if (totalBefore >= 120000) benefit.giftEvent.discount += 25000;
+      if (totalBefore >= 120000) benefit.giftEvent.discount += 25000;
+    }
 
     return benefit;
   },
