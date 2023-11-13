b8a50673e7649a1a801dc99026fbac9bf1b3482f
hongjimin00 <chunsim015@gmail.com>
2023-11-13 23:07:13 +0900

    feat: 예외 처리 handle 함수 추가

--- src/App.js
+++ src/App.js
@@ -6,32 +6,42 @@ import CalculateBenefit from "./CalculateBenefit.js";
 import CalculateDiscount from "./CalculateDiscount.js";
 
 class App {
+  async handleError(callback) {
+    try {
+      await callback();
+    } catch (error) {
+      Console.print(error.message);
+    }
+  }
+
   async run() {
-    Console.print("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+    await this.handleError(async () => {
+      Console.print("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
 
-    const visitDate = await InputView.readDate();
-    const orderList = await InputView.readMenu();
+      const visitDate = await InputView.readDate();
+      const orderList = await InputView.readMenu();
 
-    Console.print(
-      `12월 ${visitDate}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!`
-    );
+      Console.print(
+        `12월 ${visitDate}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!`
+      );
 
-    const totalBefore = CalculateTotal.calculateTotalBefore(orderList);
+      const totalBefore = CalculateTotal.calculateTotalBefore(orderList);
 
-    OutputView.printMenu(orderList);
-    OutputView.printTotalBeforeDiscount(totalBefore);
-    OutputView.printGift(totalBefore);
+      OutputView.printMenu(orderList);
+      OutputView.printTotalBeforeDiscount(totalBefore);
+      OutputView.printGift(totalBefore);
 
-    const benefitList = CalculateBenefit.calculateBenefit(
-      visitDate,
-      orderList,
-      totalBefore
-    );
-    const totalBenefit = CalculateDiscount.calculateDiscount(benefitList);
+      const benefitList = CalculateBenefit.calculateBenefit(
+        visitDate,
+        orderList,
+        totalBefore
+      );
+      const totalBenefit = CalculateDiscount.calculateDiscount(benefitList);
 
-    OutputView.printBenefits(benefitList);
-    OutputView.printTotalDiscount(totalBenefit, totalBefore);
-    OutputView.printBadge(totalBenefit);
+      OutputView.printBenefits(benefitList);
+      OutputView.printTotalDiscount(totalBenefit, totalBefore);
+      OutputView.printBadge(totalBenefit);
+    });
   }
 }
 
