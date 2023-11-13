9f47c22a2ae45f4ad23a125bd222bb4f0ef07746
hongjimin00 <chunsim015@gmail.com>
2023-11-13 14:51:46 +0900

    refactor: benefit 구조 수정

--- src/CalculateBenefit.js
+++ src/CalculateBenefit.js
@@ -4,14 +4,15 @@ const CalculateBenefit = {
   calculateBenefit(visitDate, orderLis, totalBefore) {
     const date = new Date(2023, 11, 1);
     let benefit = {
-      christmasDiscount: 0,
-      weekdayDiscount: 0,
-      weekendDiscount: 0,
-      specialDiscount: 0,
-      giftEvent: 0,
+      christmasDiscount: { name: "크리스마스 디데이 할인", discount: 0 },
+      weekdayDiscount: { name: "평일 할인", discount: 0 },
+      weekendDiscount: { name: "주말 할인", discount: 0 },
+      specialDiscount: { name: "특별 할인", discount: 0 },
+      giftEvent: { name: "증정 이벤트", discount: 0 },
     };
 
-    if (visitDate <= 25) benefit.christmasDiscount = 1000 + 100(visitDate - 1);
+    if (visitDate <= 25)
+      benefit.christmasDiscount.discount = 1000 + 100(visitDate - 1);
 
     if (date.getDay() >= 0 && date.getDay() <= 4)
       calculateDessertDiscount(orderList);
@@ -20,9 +21,9 @@ const CalculateBenefit = {
       calculateWeekendDiscount(orderList);
 
     if ([3, 10, 17, 24, 25, 31].includes(visitDate))
-      benefit.specialDiscount = 1000;
+      benefit.specialDiscount.discount = 1000;
 
-    if (totalBefore >= 120000) benefit.giftEvent += 25000;
+    if (totalBefore >= 120000) benefit.giftEvent.discount += 25000;
 
     return benefit;
   },
@@ -32,7 +33,7 @@ const CalculateBenefit = {
       const menuItem = MENU_CONSTANTS[menu.menu];
 
       if (menuItem.type === "dessert")
-        benefit.weekdayDiscount += 2023 * menu.count;
+        benefit.weekdayDiscount.discount += 2023 * menu.count;
     });
   },
 
@@ -41,7 +42,7 @@ const CalculateBenefit = {
       const menuItem = MENU_CONSTANTS[menu.menu];
 
       if (menuItem.type === "main")
-        benefit.weekendDiscount += 2023 * menu.count;
+        benefit.weekendDiscount.discount += 2023 * menu.count;
     });
   },
 };
