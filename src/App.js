2ee5f76d1e6665afd68e50803813aa7f532f7ea0
hongjimin00 <chunsim015@gmail.com>
2023-11-15 16:09:00 +0900

    refactor: 스크립트 위치 수정

--- src/App.js
+++ src/App.js
@@ -25,11 +25,8 @@ class App {
 
   async run() {
     await this.handleError(async () => {
-      Console.print("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
-
       const visitDate = await InputView.readDate();
       const orderList = await InputView.readMenu();
-
       Console.print(
         `12월 ${visitDate}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!`
       );
--- src/InputView.js
+++ src/InputView.js
@@ -5,6 +5,8 @@ import validateOrder from "./validation/validateOrder";
 
 const InputView = {
   async readDate() {
+    Console.print("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+
     const input = await Console.readLineAsync(
       "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)"
     );
